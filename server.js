d191a85a3bdbef5606ca3fb51911a24b7fd4fc0e
spolnik <jacek.spolnik@gmail.com>
2013-12-25 20:10:41 +0100

    performance improvement for get request

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ var server = http.createServer(function (req, res) {
                 items.push(item);
                 res.end('OK\n');
             });
-
             break;
         case 'GET':
-            items.forEach(function(item, i) {
-                res.write(i + ') ' + item + '\n');
-            });
-            res.end();
+            var body = items.map(function(item, i) {
+               return i + ') ' + item;
+            }).join('\n');
+
+            res.setHeader('Content-Length', Buffer.byteLength(body));
+            res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+            res.end(body);
             break;
     }
 });
