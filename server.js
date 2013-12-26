6479394131e06b9000069f06069cbd8062521f72
spolnik <jacek.spolnik@gmail.com>
2013-12-26 10:44:42 +0100

    add presentation layer - add, show items

--- server.js
+++ server.js
@@ -4,20 +4,38 @@ var TodoEngine = require('./lib/todo_engine');
 var todoEngine = new TodoEngine();
 
 var server = http.createServer(function (req, res) {
-    switch (req.method) {
-        case 'POST':
-            todoEngine.processAddItemReq(req, res);
-            break;
-        case 'GET':
-            todoEngine.processGetItemsReq(res);
-            break;
-        case 'PUT':
-            todoEngine.processUpdateItemReq(req, res);
-            break;
-        case 'DELETE':
-            todoEngine.processDeleteItemReq(req, res);
-            break;
+    if ('/' == req.url) {
+        switch (req.method) {
+            case 'POST':
+                todoEngine.addItem(req, res);
+                break;
+            case 'GET':
+                todoEngine.showItems(res);
+                break;
+            case 'PUT':
+                todoEngine.updateItem(req, res);
+                break;
+            case 'DELETE':
+                todoEngine.deleteItem(req, res);
+                break;
+            default:
+                badRequest(res);
+        }
+    } else {
+        notFound(res);
     }
 });
+
+function notFound(res) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+}
+
+function badRequest(res) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad Request');
+}
 
 server.listen(8888);
